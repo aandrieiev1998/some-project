63a3536029d606a483bb6ba9de767bc43dcaf0f3
BuildTools <unconfigured@null.spigotmc.org>
2022-09-03 21:48:28 +0200

    dsfdsf

---
+++ Assets/Imported/Mirror/Examples/Room/Scripts/NetworkRoomManagerExt.cs
@@ -0,0 +1,145 @@
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using UnityEngine;
+
+/*
+	Documentation: https://mirror-networking.gitbook.io/docs/components/network-manager
+	API Reference: https://mirror-networking.com/docs/api/Mirror.NetworkManager.html
+*/
+
+namespace Mirror.Examples.NetworkRoom
+{
+    [AddComponentMenu("")]
+    public class NetworkRoomManagerExt : NetworkRoomManager
+    {
+        [Header("Spawner Setup")]
+        [Tooltip("Reward Prefab for the Spawner")]
+        public GameObject rewardPrefab;
+
+        /// <summary>
+        /// This is called on the server when a networked scene finishes loading.
+        /// </summary>
+        /// <param name="sceneName">Name of the new scene.</param>
+        public override void OnRoomServerSceneChanged(string sceneName)
+        {
+            // spawn the initial batch of Rewards
+            if (sceneName == GameplayScene)
+                Spawner.InitialSpawn();
+        }
+
+        /// <summary>
+        /// Called just after GamePlayer object is instantiated and just before it replaces RoomPlayer object.
+        /// This is the ideal point to pass any data like player name, credentials, tokens, colors, etc.
+        /// into the GamePlayer object as it is about to enter the Online scene.
+        /// </summary>
+        /// <param name="roomPlayer"></param>
+        /// <param name="gamePlayer"></param>
+        /// <returns>true unless some code in here decides it needs to abort the replacement</returns>
+        public override bool OnRoomServerSceneLoadedForPlayer(NetworkConnectionToClient conn, GameObject roomPlayer,
+            GameObject gamePlayer)
+        {
+            PlayerScore playerScore = gamePlayer.GetComponent<PlayerScore>();
+            playerScore.index = roomPlayer.GetComponent<NetworkRoomPlayer>().index;
+            return true;
+        }
+
+        public override void OnRoomStopClient()
+        {
+            base.OnRoomStopClient();
+        }
+
+        public override void OnRoomStopServer()
+        {
+            base.OnRoomStopServer();
+        }
+
+        /*
+            This code below is to demonstrate how to do a Start button that only appears for the Host player
+            showStartButton is a local bool that's needed because OnRoomServerPlayersReady is only fired when
+            all players are ready, but if a player cancels their ready state there's no callback to set it back to false
+            Therefore, allPlayersReady is used in combination with showStartButton to show/hide the Start button correctly.
+            Setting showStartButton false when the button is pressed hides it in the game scene since NetworkRoomManager
+            is set as DontDestroyOnLoad = true.
+        */
+
+        bool showStartButton;
+
+        public override void OnRoomServerPlayersReady()
+        {
+            // calling the base method calls ServerChangeScene as soon as all players are in Ready state.
+#if UNITY_SERVER
+            base.OnRoomServerPlayersReady();
+#else
+            showStartButton = true;
+#endif
+        }
+
+        public override void OnGUI()
+        {
+            base.OnGUI();
+
+            if (allPlayersReady && showStartButton && GUI.Button(new Rect(150, 500, 120, 20), "START GAME"))
+            {
+                // set to false to hide it in the game scene
+                showStartButton = false;
+
+                ServerChangeScene(GameplayScene);
+            }
+        }
+    }
+}
+
+public class IPManager
+{
+    public static string GetIP(ADDRESSFAM Addfam)
+    {
+        //Return null if ADDRESSFAM is Ipv6 but Os does not support it
+        if (Addfam == ADDRESSFAM.IPv6 && !Socket.OSSupportsIPv6)
+        {
+            return null;
+        }
+
+        string output = "";
+
+        foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+        {
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+            NetworkInterfaceType _type1 = NetworkInterfaceType.Wireless80211;
+            NetworkInterfaceType _type2 = NetworkInterfaceType.Ethernet;
+
+            if ((item.NetworkInterfaceType == _type1 || item.NetworkInterfaceType == _type2) &&
+                item.OperationalStatus == OperationalStatus.Up)
+#endif
+            {
+                foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                {
+                    //IPv4
+                    if (Addfam == ADDRESSFAM.IPv4)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            output = ip.Address.ToString();
+                        }
+                    }
+
+                    //IPv6
+                    else if (Addfam == ADDRESSFAM.IPv6)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetworkV6)
+                        {
+                            output = ip.Address.ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+        return output;
+    }
+}
+
+public enum ADDRESSFAM
+{
+    IPv4,
+    IPv6
+}
